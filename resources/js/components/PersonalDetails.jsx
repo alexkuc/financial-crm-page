c5b5b343aec38691aec1ac0dab343a416d27f083
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2022-02-27 22:35:29 +0200

    Add simple (incomplete) table in PersonalDetails component

--- resources/js/components/PersonalDetails.jsx
+++ resources/js/components/PersonalDetails.jsx
@@ -1,5 +1,24 @@
-const PersonalDetails = () => {
-  return <section>TODO: personal details</section>;
+const PersonalDetails = ({ details }) => {
+  return (
+    <section>
+      <table>
+        <thead>
+          <tr>
+            {Object.keys(details).map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {Object.keys(details).map((key) => (
+              <td key={details[key]}>{details[key]}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
 };
 
 export default PersonalDetails;
