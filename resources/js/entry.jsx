3df2032912c6ea56ab39d5af050bd134465b2e6e
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2022-02-28 10:14:22 +0200

    Wrap <App /> in Chakra container

--- resources/js/entry.jsx
+++ resources/js/entry.jsx
@@ -1,6 +1,7 @@
 import App from './components/App';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider, Container } from '@chakra-ui/react';
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -14,7 +15,11 @@ const rootEl = document.getElementById('root');
 if (rootEl)
   ReactDOM.render(
     <BrowserRouter>
-      <App jsonUrl={rootEl.dataset.url ?? null} />
+      <ChakraProvider>
+        <Container mt={10}>
+          <App jsonUrl={rootEl.dataset.url ?? null} />
+        </Container>
+      </ChakraProvider>
     </BrowserRouter>,
     rootEl
   );
